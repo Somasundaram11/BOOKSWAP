5b7c55bafc9ddb2ebd34e962f69d38c3a40d884d
Somasundaram <somasundaramm18@gmail.com>
2026-07-14 09:54:58 +0530

    Add Django setup requirements

--- bookswap/settings.py
+++ bookswap/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'books.apps.BooksConfig',
 ]
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
